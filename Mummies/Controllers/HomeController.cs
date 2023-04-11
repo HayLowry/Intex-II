76310e89982c47bfcb8b6905a49df90f0c7ca8e2
Hayden Lowry <haylow77@gmail.com>
2023-04-11 10:13:56 -0600

    first commit of asp.net project

---
+++ Mummies/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mummies.Models;
+
+namespace Mummies.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    ////////////////////
+    /////   INDEX
+    ////////////////////
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    ////////////////////
+    /////   BURIAL LIST
+    ////////////////////
+    public IActionResult BurialList()
+    {
+        return View();
+    }
+
+    ////////////////////
+    /////   SUPERVISED
+    ////////////////////
+    public IActionResult Supervised()
+    {
+        return View();
+    }
+
+    ////////////////////
+    /////   UNSUPERVISED
+    ////////////////////
+    public IActionResult Unsupervised()
+    {
+        return View();
+    }
+
+    ////////////////////
+    /////   PRIVACY
+    ////////////////////
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
